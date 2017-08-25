8ee5b7b4fa6b55ab3bd77bd9bc28974c3ac009d8
Jake Yoon <yjaeseok@gmail.com>
2017-08-25 19:59:23 +0900

    Remove all chapter
    We re-start to study from https://kotlinlang.org

--- src/chapter2/Semicolon.kt
+++
@@ -1,8 +0,0 @@
-package chapter2
-
-import java.util.*
-
-fun main(args: Array<String>) {
-    val scanner = Scanner(System.`in`)  // do not use semicolon for a statement
-    val a = scanner.nextInt(); val b = scanner.nextInt() // use semicolon for multiple statement in one line.
-}
