fa03e77a42b16e2a2e89bf15611a52471d31e4ef
Jake Yoon <yjaeseok@gmail.com>
2017-08-25 16:15:30 +0900

    Chapter 2 Semicolon
    in Kotlin
    generally do not use Semicolon for a statement.
    Semicolon is used for multiple statement only.

---
+++ src/chapter2/Semicolon.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Semicolon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+    }
+}
